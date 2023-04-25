9d41e930185715081c55d1b060311cd02fd2a8c7
ManjiCoder <manjicoder@gmail.com>
2023-04-26 01:32:21 +0530

    footer added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Header from "./components/Header";
 import { Mulish } from "next/font/google";
+import Footer from "./components/Footer";
 
 const mulish = Mulish({
   weight: "400",
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
       <body className={mulish.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
